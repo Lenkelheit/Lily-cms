b69098d18954b551121762cb7f87c090a37014fe
Nazarii Tymtsiv <tymtsiv2710@gmail.com>
2021-05-28 20:00:35 +0300

    Test

--- LilyCmsApi/LilyCmsApi/Controllers/PagesController.cs
+++ LilyCmsApi/LilyCmsApi/Controllers/PagesController.cs
@@ -24,7 +24,7 @@ namespace LilyCmsApi.Controllers
             _pageService = pageService;
         }
 
-        // test build
+        // test build 1
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
