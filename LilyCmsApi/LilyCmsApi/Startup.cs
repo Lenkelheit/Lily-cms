82b383af46e204c1b38ac5f068300560dcd31af3
Nazarii Tymtsiv <tymtsiv2710@gmail.com>
2021-11-23 14:29:50 +0200

    Test

--- LilyCmsApi/LilyCmsApi/Startup.cs
+++ LilyCmsApi/LilyCmsApi/Startup.cs
@@ -44,7 +44,7 @@ namespace LilyCmsApi
             services.Configure<AuthGoogleConfig>(Configuration.GetSection("Authentication:Google"));
             services.AddCustomServices();
             // Test 3
-            var a = 14;
+            var a = 15;
             Console.WriteLine(a);
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
