2bbe40a49ac348c9e7486dbdda1b17df632fc35b
Nazarii Tymtsiv <tymtsiv2710@gmail.com>
2021-11-24 12:56:38 +0200

    Add loader on http requests on frontend

--- LilyCmsWebApp/src/app/app.component.ts
+++ LilyCmsWebApp/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { LoaderService } from './services/loader.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Lily CMS';
+    title = 'Lily CMS';
+
+    constructor(public loaderService: LoaderService) {
+    }
 }
